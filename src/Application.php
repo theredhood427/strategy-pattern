53e67ed354a173e932d6a54f1facd3eb5bb140a1
Romack Natividad <romack@vesanique.com.au>
2022-12-06 14:53:57 +0800

    Added Application class

---
+++ src/Application.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App;
+
+use App\Cart\Item;
+use App\Cart\ShoppingCart;
+use App\Order\Order;
+use App\Invoice\TextInvoiceStrategy;
+use App\Invoice\PDFInvoiceStrategy;
+use App\Invoice\EmailInvoiceStrategy;
+use App\Payments\CashOnDeliveryStrategy;
+use App\Payments\CreditCardStrategy;
+use App\Payments\PaypalStrategy;
+
+class Application
+{
+	public static function run()
+	{
+		$cart = new ShoppingCart();
+		$apple = new Item('APL', 'Apple', 'An apple fruit', 100);
+		$orange = new Item('ORN', 'Orange', 'An orange fruit', 200);
+		$kiwi = new Item('KIW', 'Kiwi', 'A kiwi fruit', 250);
+
+		$cart->addItem($apple, 5);
+		$cart->addItem($orange, 3);
+		$cart->addItem($kiwi, 10);
+
+		$cart->displayItems();
+
+		// Generate Invoice
+		$order = new Order('John Doe', 'johndoe@mail.com', $cart);
+
+		echo "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+		$textInvoice = new TextInvoiceStrategy();
+		$order->setInvoiceGenerator($textInvoice);
+		$order->generateInvoice();
+
+		echo "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+		$pdfInvoice = new PDFInvoiceStrategy();
+		$order->setInvoiceGenerator($textInvoice);
+		$order->generateInvoice();
+
+		echo "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+		$emailInvoice = new EmailInvoiceStrategy();
+		$order->setInvoiceGenerator($textInvoice);
+		$order->generateInvoice();
+
+		// Payment
+		echo "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+		$creditCard = new CreditCardStrategy('John Doe', '5432-1234-1231-3234', '331', '12/24');
+		$order->setPaymentMethod($creditCard);
+		$order->pay();
+
+		echo "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+		$paypal = new PaypalStrategy('johndoe@email.com', 'MYSecretPassword$$$');
+		$order->setPaymentMethod($paypal);
+		$order->pay();
+
+		echo "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+		$cod = new CashOnDeliveryStrategy('Jane Doe', '123 My Street, Suburb Town', 'Peaceful City', 777, 'Filipinas');
+		$order->setPaymentMethod($cod);
+		$order->pay();
+
+		echo "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+	}
+}
